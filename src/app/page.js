19e9279c5be535ec80032cb00539f8589cf82780
cindykandie <cindyjk6@gmail.com>
2023-08-29 20:40:34 +0300

    Add footer

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,4 @@
-import Image from 'next/image'
-import TaskItem from './components/TaskItem'
 import TaskList from './components/TaskList'
-import AddTask from './components/AddTask'
-import TaskDetails from './components/TaskDetails'
 import Footer from './components/Footer'
 
 export default function Home() {
